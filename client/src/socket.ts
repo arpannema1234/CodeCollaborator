c2371b8df4a60e34284f7082362364dfaf87ca1c
ManiacAyu <Ayush Dhoke>
2025-02-13 02:12:08 +0530

    know it

--- client/src/socket.ts
+++ client/src/socket.ts
@@ -1,7 +1,7 @@
 export const initSocket = (roomId: string): WebSocket => {
-    const backendUrl = process.env.REACT_APP_BACKEND_URL || "http://127.0.0.1:8000";
+    const backendUrl = "https://codecollaborator.onrender.com"
 
-    // Convert backend URL to WebSocket protocol
+    // Automatically switch between ws:// and wss://
     const wsProtocol = backendUrl.startsWith("https") ? "wss" : "ws";
     const wsUrl = `${wsProtocol}://${new URL(backendUrl).host}/ws/quiz/${roomId}/`;
 
